860f9cd55a992681eb86f238432676c376d378bf
Airy Magnien <amagnien@hotmail.fr>
2024-07-06 14:53:45 +0200

    refactoring

--- Code/Import/SD1_import/sd1_import.py
+++ Code/Import/SD1_import/sd1_import.py
@@ -26,40 +26,40 @@ def create_bindings(_sd1_graph: Graph):
     _sd1_graph.bind('', SD1_NAMESPACE)
 
 
-def import_sd1_data(infrastructure_path: str):
-    sd1_infra_dict = source_xml_file_to_dict(infrastructure_path)
-    trackedge_dict = sd1_infra_dict['ns0:infrastructure']['ns0:topoAreas']['ns0:topoArea']['ns0:trackEdges'][
-        'ns0:trackEdge']
-    trackedge_link_dict = sd1_infra_dict['ns0:infrastructure']['ns0:topoAreas']['ns0:topoArea']['ns0:trackEdgeLinks'][
-        'ns0:trackEdgeLink']
-
-    sd1_graph = Graph()
-    create_bindings(sd1_graph)
+def import_sd1_infra_data(infrastructure_path: str):
+    sd1_infra_dict = source_xml_file_to_dict(infrastructure_path)['ns0:infrastructure']
+    trackedge_dict = sd1_infra_dict['ns0:topoAreas']['ns0:topoArea']['ns0:trackEdges']['ns0:trackEdge']
+    trackedge_link_dict = sd1_infra_dict['ns0:topoAreas']['ns0:topoArea']['ns0:trackEdgeLinks']['ns0:trackEdgeLink']
 
     # RSM import statement; not used
     # sd1_graph.add((URIRef(SD1_NAMESPACE), OWL.imports, URIRef(RSM_TOPOLOGY_NAMESPACE)))
 
     topology_graph = TopologyGraph(sd1_graph)
+    generate_linear_elements_from_track_edges(trackedge_dict, topology_graph)
+    generate_connections_from_track_edge_links(trackedge_link_dict, topology_graph)
 
-    # Track Edges to Linear Elements
-    for trackedge in trackedge_dict:
+
+def generate_linear_elements_from_track_edges(_trackedge_dict: dict, _topology_graph: TopologyGraph):
+    for trackedge in _trackedge_dict:
         sd1id = trackedge['@id']
         length = trackedge['@length']
         unit_repr = 'qudt'
-        topology_graph.add_trackedge_as_linearelement(sd1id, length, SD1_NAMESPACE, unit_repr)
-    topology_graph.create_ports()
+        _topology_graph.add_trackedge_as_linearelement(sd1id, length, SD1_NAMESPACE, unit_repr)
+    _topology_graph.create_ports()
 
-    # Track Edge Links to property: connected with
-    for trackedge_link in trackedge_link_dict:
+
+def generate_connections_from_track_edge_links(_trackedge_link_dict: dict, _topology_graph: TopologyGraph):
+    for trackedge_link in _trackedge_link_dict:
         trackedge_a = trackedge_link['@trackEdgeA']
         trackedge_b = trackedge_link['@trackEdgeB']
         position_on_a = 0 if trackedge_link['@startOfA'] == "true" else 1
         position_on_b = 0 if trackedge_link['@startOfB'] == "true" else 1
-        topology_graph.add_connection(trackedge_a, position_on_a, trackedge_b, position_on_b, SD1_NAMESPACE)
-
-    sd1_graph.print()
+        _topology_graph.add_connection(trackedge_a, position_on_a, trackedge_b, position_on_b, SD1_NAMESPACE)
 
 
 if __name__ == '__main__':
+    sd1_graph = Graph()
+    create_bindings(sd1_graph)
     infra_path = data_root + "/scheibenberg/infra_v0.4.2.xml"
-    import_sd1_data(infra_path)
+    import_sd1_infra_data(infra_path)
+    sd1_graph.print()
