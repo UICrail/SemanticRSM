3c91b510ca29db897cd0ee696efff6f62b9a784a
Airy Magnien <amagnien@hotmail.fr>
2024-07-08 16:43:54 +0200

    refactoring

--- Code/Import/SD1_import/sd1_import.py
+++ Code/Import/SD1_import/sd1_import.py
@@ -11,12 +11,12 @@ from sd1_topology_import import TopologyGraph
 SD1_NAMESPACE = Namespace("http://example.org/scheibenberg/")
 
 
-def source_xml_file_to_dict(path: str) -> dict:
+def get_infra_dict_from_xml(path: str) -> dict:
     # Et.register_namespace(prefix = "", uri=SD1_NAMESPACE)
     xml_data = Et.parse(path).getroot()
     xml_string = Et.tostring(xml_data, encoding="utf-8", method="xml")  # needed to avoid invalid token error
-    sd1dict = xmltodict.parse(xml_string)
-    return sd1dict
+    infra_dict = xmltodict.parse(xml_string)['ns0:infrastructure']
+    return infra_dict
 
 
 def create_bindings(_sd1_graph: Graph):
@@ -26,21 +26,9 @@ def create_bindings(_sd1_graph: Graph):
     _sd1_graph.bind('', SD1_NAMESPACE)
 
 
-def import_sd1_infra_data(infrastructure_path: str):
-    sd1_infra_dict = source_xml_file_to_dict(infrastructure_path)['ns0:infrastructure']
-    trackedge_dict = sd1_infra_dict['ns0:topoAreas']['ns0:topoArea']['ns0:trackEdges']['ns0:trackEdge']
-    trackedge_link_dict = sd1_infra_dict['ns0:topoAreas']['ns0:topoArea']['ns0:trackEdgeLinks']['ns0:trackEdgeLink']
-
-    # RSM import statement; not used
-    # sd1_graph.add((URIRef(SD1_NAMESPACE), OWL.imports, URIRef(RSM_TOPOLOGY_NAMESPACE)))
-
-    topology_graph = TopologyGraph(sd1_graph)
-    generate_linear_elements_from_track_edges(trackedge_dict, topology_graph)
-    generate_connections_from_track_edge_links(trackedge_link_dict, topology_graph)
-
-
-def generate_linear_elements_from_track_edges(_trackedge_dict: dict, _topology_graph: TopologyGraph):
-    for trackedge in _trackedge_dict:
+def generate_linear_elements_from_track_edges(_infra_dict: dict, _topology_graph: TopologyGraph):
+    trackedge_dict = _infra_dict['ns0:topoAreas']['ns0:topoArea']['ns0:trackEdges']['ns0:trackEdge']
+    for trackedge in trackedge_dict:
         sd1id = trackedge['@id']
         length = trackedge['@length']
         unit_repr = 'qudt'
@@ -48,13 +36,37 @@ def generate_linear_elements_from_track_edges(_trackedge_dict: dict, _topology_g
     _topology_graph.create_ports()
 
 
-def generate_connections_from_track_edge_links(_trackedge_link_dict: dict, _topology_graph: TopologyGraph):
-    for trackedge_link in _trackedge_link_dict:
+def generate_connections_from_track_edge_links(_infra_dict: dict, _topology_graph: TopologyGraph):
+    trackedge_link_dict = _infra_dict['ns0:topoAreas']['ns0:topoArea']['ns0:trackEdgeLinks']['ns0:trackEdgeLink']
+    for trackedge_link in trackedge_link_dict:
         trackedge_a = trackedge_link['@trackEdgeA']
         trackedge_b = trackedge_link['@trackEdgeB']
         position_on_a = 0 if trackedge_link['@startOfA'] == "true" else 1
         position_on_b = 0 if trackedge_link['@startOfB'] == "true" else 1
         _topology_graph.add_connection(trackedge_a, position_on_a, trackedge_b, position_on_b, SD1_NAMESPACE)
+
+
+def generate_navigabilities_at_simple_points(_infra_dict: dict, _topology_graph: TopologyGraph):
+    simple_point_dict = _infra_dict['ns0:functionalAreas']['ns0:functionalArea']['ns0:simplePoints'][
+        'ns0:simplePoint']
+    for simple_point in simple_point_dict:
+        point_left = simple_point['@pointLeft']
+        point_right = simple_point['@pointRight']
+
+
+#######################################################################################################################
+# Main routine
+#######################################################################################################################
+def import_sd1_infra_data(infrastructure_path: str):
+    sd1_infra_dict = get_infra_dict_from_xml(infrastructure_path)
+
+    # RSM import statement; not used
+    # sd1_graph.add((URIRef(SD1_NAMESPACE), OWL.imports, URIRef(RSM_TOPOLOGY_NAMESPACE)))
+
+    topology_graph = TopologyGraph(sd1_graph)
+    generate_linear_elements_from_track_edges(sd1_infra_dict, topology_graph)
+    generate_connections_from_track_edge_links(sd1_infra_dict, topology_graph)
+    generate_navigabilities_at_simple_points(sd1_infra_dict, topology_graph)
 
 
 if __name__ == '__main__':
