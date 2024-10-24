389735631317cbc7505262c1973e0dc6b8fa0737
Airy Magnien <amagnien@hotmail.fr>
2024-10-24 11:54:17 +0200

    added a comment for testing GitHub push access

--- Code/Namespaces.py
+++ Code/Namespaces.py
@@ -5,3 +5,5 @@ RSM_TOPOLOGY = Namespace("http://cdm.ovh/rsm/topology/topology#")
 RSM_GEOSPARQL_ADAPTER = Namespace("http://cdm.ovh/rsm/adapters/geosparql_adapter#")
 RSM_COMPLETE = Namespace("http://cdm.ovh/rsm/rsm_complete#")
 WORK = Namespace("http://cdm.ovh/examples/Ventimiglia-Albenga/Ventimiglia-Albenga#")
+
+# useless comment added for testing Git
