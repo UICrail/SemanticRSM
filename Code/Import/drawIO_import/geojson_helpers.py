8372b01643936116f35d96d55555cd957a4607cb
Airy Magnien <amagnien@hotmail.fr>
2024-11-06 13:22:07 +0100

    refactoring (again) geojson_helpers.py. Looks OK now.

--- Code/Import/drawIO_import/geojson_helpers.py
+++ Code/Import/drawIO_import/geojson_helpers.py
@@ -13,45 +13,55 @@ CANVAS_ORIENTATION = -1
 transformer = Transformer.from_crs(DEFAULT_PROJECTION, DEFAULT_GEO_REFERENCE)
 
 
-def create_geojson_point(x: float | str, y: float | str) -> object:
+def convert_multiple_canvas_coords_to_lonlat(*coords: tuple[str | float, str | float]) -> tuple[tuple[float, float], ...]:
     """
+    Convert Cartesian coordinates to longitude and latitude.
 
-    :param x:
-    :param y:
-    :return:
+    :param coords: array of coordinate pairs (X,Y) [, (X1, Y1)...] on some canvas
+    :return: Tuple of longitude and latitude pairs.
     """
-    return geojson.Point(cartesian_to_lonlat((float(x), float(y))))
+    return tuple(_convert_canvas_coords_to_lonlat(coord) for coord in coords)
 
 
-def cartesian_to_lonlat(*coords: tuple[str | float, str | float]):
+def _convert_canvas_coords_to_lonlat(coord: tuple[str | float, str | float]) -> tuple[float, float]:
     """
-
-    :param coords: array of coordinate pairs (X,Y) [, (X1, Y1)...]
-    :return: same but expressed in (longitude, latitude)
+    Convert a single coordinate pair from Cartesian (on canvas) to longitude and latitude.
+    Note that the ordering conventions for coordinates differ...
+    :param coord: Coordinate pair (X, Y) on Canvas
+    :return: Converted coordinate pair (longitude, latitude)
     """
+    return transformer.transform(*_convert_canvas_coords_to_projection(coord))
 
-    result = []
-    for coord in coords:
-        result.append(transformer.transform(CANVAS_ORIENTATION * float(coord[1]) + DEFAULT_CENTER_COORDS[1],
-                                            float(coord[0]) + DEFAULT_CENTER_COORDS[0]))
-    return tuple(result)
+
+def _convert_canvas_coords_to_projection(canvas_coord: tuple[str | float, str | float]) -> tuple[float, float]:
+    return CANVAS_ORIENTATION * float(canvas_coord[1]) + DEFAULT_CENTER_COORDS[1], \
+           float(canvas_coord[0]) + DEFAULT_CENTER_COORDS[0]
 
 
 def create_geojson_linestring(source: tuple[str | float, str | float], target: tuple[str | float, str | float],
                               *waypoints: tuple[str | float, str | float]) -> LineString:
     """
+    Create a GeoJSON LineString from source, target, and optional waypoints.
 
-    :param source: (X,Y) coordinate pair
-    :param target: ditto
-    :param waypoints: same, between source and target; the sequence source - *waypoints - target must be correct
-    :return: GeoJSON Linestring object
+    :param source: (X, Y) coordinate pair (on canvas) for the source.
+    :param target: (X, Y) coordinate pair (on canvas) for the target.
+    :param waypoints: Optional waypoints between source and target.
+    :return: GeoJSON LineString object.
     """
-    lon_lat_array = ()
-    if waypoints:
-        lon_lat_array = cartesian_to_lonlat(*waypoints)
     way_coords = (
-        cartesian_to_lonlat(source),
-        *lon_lat_array,
-        cartesian_to_lonlat(target)
+        convert_multiple_canvas_coords_to_lonlat(source),
+        *convert_multiple_canvas_coords_to_lonlat(*waypoints),
+        convert_multiple_canvas_coords_to_lonlat(target)
     )
     return geojson.LineString(way_coords)
+
+
+def create_geojson_point(x: float | str, y: float | str) -> geojson.Point:
+    """
+    Create a GeoJSON point from Cartesian coordinates on canvas.
+
+    :param x: X coordinate.
+    :param y: Y coordinate.
+    :return: GeoJSON Point object.
+    """
+    return geojson.Point(_convert_canvas_coords_to_lonlat((float(x), float(y))))
