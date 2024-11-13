99eef268acc8b21026417544428ba7b8698c14ab
Airy Magnien <32216240+Airy59@users.noreply.github.com>
2024-11-13 10:36:50 +0100

    File I/O for railML 3.2 import, refactored

--- Code/Import/railML32_import/Railml32ToSrsm.py
+++ Code/Import/railML32_import/Railml32ToSrsm.py
@@ -11,24 +11,26 @@ RAILML32_OUTPUT_FOLDER = os.path.join(current_dir, 'TestOutputs')  # default out
 class Railml32ToRsm:
     def __init__(self):
         self._root = None
-        self._output_path = ''
+        self._input_path = None
         self._output_directory = None
+        self._output_path = None
         self._graph = None
 
     def process_railML32(self, input_path: str, output_directory: str, short_name: str = ''):
         """
-        :param short_name:
+        :param short_name: for the output file. If not provided, the input file base name shall be kept.
         :param input_path:
         :param output_directory:
         :return:
         """
+        self._input_path = input_path
         self.output_directory = output_directory
-        self._output_path = self._generate_output_path(output_directory, input_path, short_name)
-        self.load_source(input_path)
+        self.output_path = short_name
+        self._load_source(input_path)
         self._graph = rdflib.Graph()
         self._save_graph_to_file()
 
-    def load_source(self, path: str) -> str:
+    def _load_source(self, path: str) -> str:
         """
         :param path: str, path to the source code file to be loaded
         :return: str, message indicating success or failure
@@ -52,30 +54,30 @@ class Railml32ToRsm:
 
     @property
     def output_directory(self):
-        """
-        Property to get the output directory. Ensures the directory exists (creates it if necessary).
-        :return: str, the output directory path
-        """
-        if self._output_directory:
-            if not os.path.exists(self._output_directory):
-                try:
-                    os.makedirs(self._output_directory)
-                except OSError as e:
-                    print(f"ERROR: could not create {self._output_directory}: {e}")
-        else:
-            self._output_directory = RAILML32_OUTPUT_FOLDER
         return self._output_directory
 
     @output_directory.setter
     def output_directory(self, value: str):
-        self._output_directory = value
-
-    @staticmethod
-    def _generate_output_path(output_directory: str, input_path: str, short_name: str) -> str:
-        if short_name:
-            return os.path.join(output_directory, f"{short_name}.ttl")
+        if value:
+            self._output_directory = value
         else:
-            return os.path.join(output_directory, f"{os.path.basename(input_path)}.ttl")
+            self._output_directory = RAILML32_OUTPUT_FOLDER
+        if not os.path.exists(self._output_directory):
+            try:
+                os.makedirs(self._output_directory)
+            except OSError as e:
+                print(f"ERROR: could not create {self._output_directory}: {e}")
+
+    @property
+    def output_path(self):
+        return self._output_path
+
+    @output_path.setter
+    def output_path(self, value: str):
+        if value:
+            self._output_path = os.path.join(self.output_directory, f"{value}.ttl")
+        else:
+            self._output_path = os.path.join(self.output_directory, f"{os.path.basename(value)}.ttl")
 
 
 if __name__ == "__main__":
